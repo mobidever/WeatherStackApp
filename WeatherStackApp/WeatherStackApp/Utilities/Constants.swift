c4f376688ba2d1d055a6a9f0ba441cf99cbb9548
mobidever <pramod.mobidev@gmail.com>
2024-11-01 17:21:20 +0530

    Updated api_key to avoid monthly limits
    Updated api_key to avoid monthly limits of API Call.

--- WeatherStackApp/WeatherStackApp/Utilities/Constants.swift
+++ WeatherStackApp/WeatherStackApp/Utilities/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // api key used to load services
-let api_key : String = "18958cb350fe932cdd4532589ea43ebb"
+let api_key : String = "f8e03f315d56dd4ced3c5e74d11ff124"
 
 //URLs to load services from WeatherStack.
 struct Urls {
