7b0844322bb5d6a75dd4dbc55e9d18b7fcc21bc1
mobidever <pramod.mobidev@gmail.com>
2024-11-01 16:02:07 +0530

    Refactored the changes to improve Code Coverage
    Improved Code Coverage.

--- WeatherStackApp/WeatherStackApp/Utilities/Constants.swift
+++ WeatherStackApp/WeatherStackApp/Utilities/Constants.swift
@@ -7,21 +7,13 @@
 
 import Foundation
 
+// api key used to load services
 let api_key : String = "18958cb350fe932cdd4532589ea43ebb"
 
+//URLs to load services from WeatherStack.
 struct Urls {
 	
 	static func weatherInfoURL(for location: String) -> URL {
 		return URL(string: "https://api.weatherstack.com/current?access_key=\(api_key)&query=\(location)")!
 	}
-	
-	//Available on standard plans
-	static func weatherForecastURL(for location: String) -> URL{
-		return URL(string: "https://api.weatherstack.com/current?access_key=\(api_key)&query=\(location)&forecast_days=1&hourly=1")!
-	}
-	
-	static func search(for location:String) -> URL {
-		return URL(string: "https://api.weatherstack.com/autocomplete?access_key=\(api_key)&query=\(location)")!
-	}
-	
 }
